1b59bfc5e9e9c06eafd481e7183f7baf75e4c3c5
kelvynnjs <kelvynn4@hotmail.com>
2021-04-25 18:06:07 -0300

    Início Projeto Helpdesk

---
+++ Helpdesk_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HelpdeskAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Helpdesk_app'
---
+++ Helpdesk_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+# Create your views here.  SÃO AS VISÕES DO APP
+
+
+#Cria a pagina inicial
+def home(request):
+    return HttpResponse()
