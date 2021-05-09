edbcad13cc969c5c90ecbeda95937e72bb2685ff
kelvynnjs <kelvynn4@hotmail.com>
2021-05-09 16:06:56 -0300

    Correção em dir Static, bootstrap e javascript

--- helpdesk/settings.py
+++ helpdesk/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-  #  'Helpdesk_app.apps.HelpdeskAppConfig',
-    'Helpdesk_app',
+    'Helpdesk_app'
 ]
 
 MIDDLEWARE = [
